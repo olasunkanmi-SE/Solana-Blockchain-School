be04b5de1f927610712e3a44a0c56697aa88d229
Olasunkanmi Oyinlola <olasunkanmi@regovtech.com>
2024-09-27 15:31:52 +0800

    feat(school): Implement School state and update README
    Add School struct and SchoolTrait in new state module
    Update README with new School account fields and Course details
    Rename Class to Course in README for consistency
    Implement methods for managing school state (e.g., incrementing counts)
    Add student_count field to School struct

---
+++ school/programs/school/src/state/school.rs
@@ -0,0 +1,71 @@
+use anchor_lang::prelude::*;
+
+#[account]
+pub struct School {
+    authority: Pubkey,
+    enrolment_fee: u64,
+    class_count: u64,
+    book_count: u64,
+    student_count: u64,
+}
+
+pub trait SchoolTrait {
+    fn new(authority: Pubkey, enrolment_fee: u64) -> Self;
+    fn get_authority(&self) -> Pubkey;
+    fn get_enrolment_fee(&self) -> u64;
+    fn class_count(&self) -> u64;
+    fn book_count(&self) -> u64;
+    fn student_count(&self) -> u64;
+    fn increment_class_count(&mut self);
+    fn increment_book_count(&mut self);
+    fn increment_student_count(&mut self);
+    fn set_enrolment_fee(&mut self, new_fee: u64);
+}
+
+impl SchoolTrait for School {
+    fn new(authority: Pubkey, enrolment_fee: u64) -> Self {
+        School {
+            authority,
+            enrolment_fee,
+            class_count: 0,
+            book_count: 0,
+            student_count: 0,
+        }
+    }
+
+    fn get_authority(&self) -> Pubkey {
+        self.authority
+    }
+
+    fn get_enrolment_fee(&self) -> u64 {
+        self.enrolment_fee
+    }
+
+    fn class_count(&self) -> u64 {
+        self.class_count
+    }
+
+    fn book_count(&self) -> u64 {
+        self.book_count
+    }
+
+    fn student_count(&self) -> u64 {
+        self.student_count
+    }
+
+    fn increment_class_count(&mut self) {
+        self.class_count += 1
+    }
+
+    fn increment_book_count(&mut self) {
+        self.book_count += 1
+    }
+
+    fn increment_student_count(&mut self) {
+        self.student_count += 1
+    }
+
+    fn set_enrolment_fee(&mut self, new_fee: u64) {
+        self.enrolment_fee = new_fee;
+    }
+}
